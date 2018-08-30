1014c506605afa2ab4bca095ad5eddad34ba0341
tannyque <tannyque1992@gmail.com>
2018-08-30 14:34:58 +0100

    Creates object of name, meaning and height :tada:

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+const Munros = require('./models/munros.js');
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript Loaded');
+  const munros = new Munros();
+  munros.getData();
 })
---
+++ src/models/munros.js
@@ -0,0 +1,30 @@
+const RequestHelper = require('../helpers/request_helper.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+
+const Munros = function () {
+  this.munros = [];
+};
+
+Munros.prototype.getData = function () {
+  const requestHelper = new RequestHelper('https://munroapi.herokuapp.com/api/munros');
+  requestHelper.get((data) => {
+    this.modelMunros(data);
+    PubSub.publish('Munros:data-ready', this.munros);
+    // gets name
+    // console.log(this.munros);
+  })
+};
+
+Munros.prototype.modelMunros = function (data) {
+  this.munros = data.map((munro) => {
+    return {
+      name: munro.name,
+      height: munro.height,
+      meaning: munro.meaning
+    };
+  })
+};
+
+
+module.exports = Munros;
